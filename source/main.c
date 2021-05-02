f1c52068ae825e56633ea5775b1563ac3a041c67
maciel310 <maciel310@gmail.com>
2021-05-01 19:30:52 -0600

    Move serial-related functions to a separate file.

--- source/main.c
+++ source/main.c
@@ -5,7 +5,9 @@
 #include "protos/world_object.pb.h"
 #include <pb_encode.h>
 #include <pb_decode.h>
+
 #include "world_objects.h"
+#include "serial.h"
 
 OBJ_ATTR sprite[128];
 
@@ -33,41 +35,6 @@ void updateWorldObjectSpriteEntry(int i, struct world_object *o) {
   obj_set_pos(&sprite[i], o->x, o->y);
 }
 
-bool decode_world_object(pb_istream_t *stream, const pb_field_t *field, void **arg) {
-  WorldObject message = WorldObject_init_zero;
-  pb_decode(stream, WorldObject_fields, &message);
-  update_world_object(message);
-
-  return true;
-}
-
-size_t message_length = 0;
-size_t expected_message_length = 0;
-u8 buffer[128];
-void handle_serial() {
-  u32 data = REG_SIODATA32;
-  if (expected_message_length == 0) {
-    expected_message_length = data;
-    message_length = 0;
-    return;
-  }
-
-  buffer[message_length++] = ((data >> 24) & 0xff);
-  buffer[message_length++] = ((data >> 16) & 0xff);
-  buffer[message_length++] = ((data >> 8) & 0xff);
-  buffer[message_length++] = (data & 0xff);
-
-  if (message_length >= expected_message_length) {
-    ServerUpdate message = ServerUpdate_init_zero;
-    pb_istream_t stream = pb_istream_from_buffer(buffer, expected_message_length);
-    message.world_object.funcs.decode = decode_world_object;
-    pb_decode(&stream, ServerUpdate_fields, &message);
-
-    expected_message_length = 0;
-    message_length = 0;
-  }
-}
-
 int main() {
 
   REG_RCNT = 0;
@@ -75,7 +42,7 @@ int main() {
 
   irq_init(NULL);
   irq_enable(II_VBLANK);
-  irq_add(II_SERIAL, handle_serial);
+  serial_init();
 
   oam_init(sprite, 128);
 
---
+++ source/serial.h
@@ -0,0 +1,14 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <tonc.h>
+#include "protos/network_messages.pb.h"
+#include "protos/world_object.pb.h"
+#include <pb_encode.h>
+#include <pb_decode.h>
+
+void serial_init();
+bool decode_world_object(pb_istream_t *stream, const pb_field_t *field, void **arg);
+void handle_serial();
+
+#endif
