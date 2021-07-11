a45a2c0804c926c2aae236b2e627e5e9ba0e970e
maciel310 <maciel310@gmail.com>
2021-07-10 20:53:04 -0600

    Refactor object interaction logic to take the sprite size into account when looking for which object the player wants to interact with. Now trees and other resources can be collected from any side of the object.

--- source/world_objects.c
+++ source/world_objects.c
@@ -8,6 +8,42 @@ struct world_object* world_object_head = NULL;
 
 bool sprite_collision_map[64][64];
 
+
+const u32 sprite_type_lut[12] = {
+  ATTR0_SQUARE, ATTR0_SQUARE, ATTR0_SQUARE, ATTR0_SQUARE,
+  ATTR0_WIDE, ATTR0_WIDE, ATTR0_WIDE, ATTR0_WIDE,
+  ATTR0_TALL, ATTR0_TALL, ATTR0_TALL, ATTR0_TALL
+};
+
+const u32 sprite_size_lut[12] = {
+  ATTR1_SIZE_8x8,
+  ATTR1_SIZE_16x16,
+  ATTR1_SIZE_32x32,
+  ATTR1_SIZE_64x64,
+
+  ATTR1_SIZE_16x8,
+  ATTR1_SIZE_32x8,
+  ATTR1_SIZE_32x16,
+  ATTR1_SIZE_64x32,
+
+  ATTR1_SIZE_8x16,
+  ATTR1_SIZE_8x32,
+  ATTR1_SIZE_16x32,
+  ATTR1_SIZE_32x64,
+};
+
+const u32 sprite_tile_width_lut[12] = {
+  1, 2, 4, 8,
+  2, 4, 4, 8,
+  1, 1, 2, 4,
+};
+
+const u32 sprite_tile_height_lut[12] = {
+  1, 2, 4, 8,
+  1, 1, 2, 4,
+  2, 4, 4, 8,
+};
+
 void convert_world_object(WorldObject proto_object, struct world_object* struct_object) {
   struct_object->object_id = proto_object.object_id;
   struct_object->sprite_id = proto_object.sprite_id;
@@ -98,79 +134,55 @@ bool update_world_object(WorldObject o) {
 
 // Returns the world_object next to the given position, or 0 if none found.
 u32 find_next_to(s32 x, s32 y, Direction direction) {
-  s32 object_x_min;
-  s32 object_x_max;
-  s32 object_y_min;
-  s32 object_y_max;
+  s32 x_tile = x / 8;
+  s32 y_tile = y / 8;
+
+  s32 object_x_tile_min;
+  s32 object_x_tile_max;
+  s32 object_y_tile_min;
+  s32 object_y_tile_max;
   if (direction == Direction_UP) {
-    object_x_min = x - 8;
-    object_x_max = x + 16;
-    object_y_min = y - 16;
-    object_y_max = y + 8;
+    object_x_tile_min = x_tile - 1;
+    object_x_tile_max = x_tile + 1;
+    object_y_tile_min = y_tile + 1;
+    object_y_tile_max = y_tile + 1;
   } else if (direction == Direction_DOWN) {
-    object_x_min = x - 8;
-    object_x_max = x + 16;
-    object_y_min = y + 8;
-    object_y_max = y + 24;
+    object_x_tile_min = x_tile - 1;
+    object_x_tile_max = x_tile + 1;
+    object_y_tile_min = y_tile + 3;
+    object_y_tile_max = y_tile + 3;
   } else if (direction == Direction_LEFT) {
-    object_x_min = x - 16;
-    object_x_max = x - 8;
-    object_y_min = y - 8;
-    object_y_max = y + 8;
+    object_x_tile_min = x_tile;
+    object_x_tile_max = x_tile;
+    object_y_tile_min = y_tile - 1;
+    object_y_tile_max = y_tile + 2;
   } else {
-    object_x_min = x + 8;
-    object_x_max = x + 16;
-    object_y_min = y - 8;
-    object_y_max = y + 8;
+    object_x_tile_min = x_tile + 2;
+    object_x_tile_max = x_tile + 2;
+    object_y_tile_min = y_tile - 1;
+    object_y_tile_max = y_tile + 1;
   }
 
   struct world_object* current = world_object_head;
   while (current != NULL) {
-    if (current->is_on_screen
-        && current->x >= object_x_min && current->x <= object_x_max
-        && current->y >= object_y_min && current->y <= object_y_max) {
-      return current->object_id;
+    if (current->is_on_screen) {
+      for (s32 x = 0; x < sprite_tile_width_lut[current->sprite_size]; x++) {
+        for (s32 y = 0; y < sprite_tile_height_lut[current->sprite_size]; y++) {
+          s32 object_tile_x = current->dest_x / 8 + x;
+          s32 object_tile_y = current->dest_y / 8 + y;
+
+          if (object_tile_x >= object_x_tile_min && object_tile_x <= object_x_tile_max
+              && object_tile_y >= object_y_tile_min && object_tile_y <= object_y_tile_max) {
+            return current->object_id;
+          }
+        }
+      }
     }
 
     current = current->next;
   }
   return 0;
 }
-
-const u32 sprite_type_lut[12] = {
-  ATTR0_SQUARE, ATTR0_SQUARE, ATTR0_SQUARE, ATTR0_SQUARE,
-  ATTR0_WIDE, ATTR0_WIDE, ATTR0_WIDE, ATTR0_WIDE,
-  ATTR0_TALL, ATTR0_TALL, ATTR0_TALL, ATTR0_TALL
-};
-
-const u32 sprite_size_lut[12] = {
-  ATTR1_SIZE_8x8,
-  ATTR1_SIZE_16x16,
-  ATTR1_SIZE_32x32,
-  ATTR1_SIZE_64x64,
-
-  ATTR1_SIZE_16x8,
-  ATTR1_SIZE_32x8,
-  ATTR1_SIZE_32x16,
-  ATTR1_SIZE_64x32,
-
-  ATTR1_SIZE_8x16,
-  ATTR1_SIZE_8x32,
-  ATTR1_SIZE_16x32,
-  ATTR1_SIZE_32x64,
-};
-
-const u32 sprite_tile_width_lut[12] = {
-  1, 2, 4, 8,
-  2, 4, 4, 8,
-  1, 1, 2, 4,
-};
-
-const u32 sprite_tile_height_lut[12] = {
-  1, 2, 4, 8,
-  1, 1, 2, 4,
-  2, 4, 4, 8,
-};
 
 void regenerate_sprite_collision_map() {
   u32 x = 0;
